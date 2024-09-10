75339fcdc9db781623a2830e69d24ccad09f2192
Kelseyayuya <167401004+Kelseyayuya@users.noreply.github.com>
2024-09-10 17:17:32 +0300

    Add files via upload

---
+++ Assignment tables.sql
@@ -0,0 +1,79 @@
+CREATE TABLE students(
+	gender VARCHAR(30),
+	year INT
+);
+
+SELECT*FROM students;
+
+INSERT INTO students(gender,year)
+VALUES('girl',2006);
+
+ SELECT*FROM students; 
+
+ INSERT INTO students(gender,year)
+ VALUES
+ 	('Girl',2009),
+	 ('Boy',2000);
+
+SELECT*FROM students;
+
+DROP TABLE students;
+
+SELECT*FROM students;
+
+CREATE TABLE customers (
+  customer_id SERIAL NOT NULL PRIMARY KEY,
+  customer_name VARCHAR(255),
+  contact_name VARCHAR(255),
+  address VARCHAR(255),
+  city VARCHAR(255),
+  postal_code VARCHAR(255),
+  country VARCHAR(255)
+);
+
+SELECT*FROM customers;
+
+
+ INSERT INTO customers (customer_name, contact_name, address, city, postal_code, country)
+VALUES
+  ('Alfreds Futterkiste', 'Maria Anders', 'Obere Str. 57', 'Berlin', '12209', 'Germany'),
+  ('Ana Trujillo Emparedados y helados', 'Ana Trujillo', 'Avda. de la Constitucion 2222', 'Mexico D.F.', '05021', 'Mexico'),
+  ('Antonio Moreno Taquera', 'Antonio Moreno', 'Mataderos 2312', 'Mexico D.F.', '05023', 'Mexico'),
+  ('Around the Horn', 'Thomas Hardy', '120 Hanover Sq.', 'London', 'WA1 1DP', 'UK');
+
+SELECT*FROM customers;
+
+DROP TABLE customers;
+
+SELECT*FROM customers;
+
+CREATE TABLE products (
+  product_id SERIAL NOT NULL PRIMARY KEY,
+  product_name VARCHAR(255),
+  category_id INT,
+  unit VARCHAR(255),
+  price DECIMAL(10, 2)
+);
+
+SELECT*FROM products;
+
+INSERT INTO products (product_id, product_name, category_id, unit, price)
+VALUES
+  (1, 'Chais', 1, '10 boxes x 20 bags', 18),
+  (2, 'Chang', 1, '24 - 12 oz bottles', 19),
+  (3, 'Aniseed Syrup', 2, '12 - 550 ml bottles', 10),
+  (4, 'Chef Antons Cajun Seasoning', 2, '48 - 6 oz jars', 22);
+
+ SELECT*FROM products;
+
+ DROP TABLE products;
+ 
+
+  
+  
+
+  
+  
+
+
+	
